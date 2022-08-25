572137206e002f9181975518c57d247ca247162a
Keefe Overby <you@example.com>
2022-08-25 18:42:19 -0500

    Working on components - WrappedUsername

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Link from 'next/link';
+import { useInView } from 'react-intersection-observer';
+import styles from '../styles/fader.module.scss';
 
 const Header = () => {
+    const { ref: headerSectionRef, inView: headerSectionIsVisible } = useInView();
     return (
         <Link href="/">
-            <h1 className='w-screen text-center items-center font-bold gradient-bg-site bg-clip-text text-transparent text-9xl cursor-pointer'>
-                minting dapp template
-            </h1>
+            <div ref={headerSectionRef} className={headerSectionIsVisible ? styles.fadeIn : styles.fadeOut}>
+                <div className="flex h-screen flex-col justify-center mb-2 py-2">
+                    <h1 className='fixed w-screen text-center items-center font-bold gradient-bg-site bg-clip-text text-transparent text-9xl cursor-pointer'>
+                        minting dapp template
+                    </h1>
+                </div>
+            </div>
         </Link>
     )
 }
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,7 @@
 export { default as Header } from './Header';
+export { default as Mint } from './Mint';
+export { default as About } from './About';
+export { default as RoadMap } from './RoadMap';
+export { default as Footer } from './Footer';
 export { default as Layout } from './Layout';
+export { default as Loader } from './Loader';
