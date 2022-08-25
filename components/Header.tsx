785a998a7f313cd66e6d3d4b3cfc1cfedf111c7b
Keefe Overby <you@example.com>
2022-08-25 16:52:39 -0500

    Added and configured files - WrappedUsername

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <Link href="/">
+            <h1 className='w-screen text-center items-center font-bold gradient-bg-site bg-clip-text text-transparent text-9xl cursor-pointer'>
+                minting dapp template
+            </h1>
+        </Link>
+    )
 }
 
 export default Header
---
+++ context/constants.ts
@@ -0,0 +1,19 @@
+// Update smart contract address and scanLink when deployed blockchain.
+export const address = ""; // TODO: add smart contract address here
+export const scanLink = ""; // TODO: add link to etherscan.io
+export const discordLink = ""; // Add your discord link here
+export const githubLink = ""; // TODO: add github link
+
+// contract abi, update when deployed to blockchain.
+export const abi = [
+  // solidity json abi i.e. output of `solc --combined-json abi,bin,interface`
+  /**{
+    inputs: [],
+    name: "_tokenIdCounter",
+    outputs: [{ internalType: "uint256", name: "_value", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },*/
+  // human-readable abi i.e. function name, inputs, outputs
+  //"function safeMint() public payable",
+];
