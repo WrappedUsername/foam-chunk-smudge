1ee2679e895fdc1ef2e7f655eb8c36a9ee3ccbfc
Keefe Overby <you@example.com>
2022-08-25 15:38:36 -0500

    Initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from ".";
+
+const Layout = ({ children } : {children:any}) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
+}
+
+export default Layout
