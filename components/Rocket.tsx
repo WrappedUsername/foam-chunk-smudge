3963c1f7c05eb9ef0115cd9a6ae9e8a07f4dc0c9
Keefe Overby <you@example.com>
2022-09-25 16:37:57 -0500

    Working on components - WrappedUsername

--- components/Rocket.tsx
+++ components/Rocket.tsx
@@ -1,14 +1,12 @@
 import React, { useRef } from 'react';
-import { useInView } from 'react-intersection-observer';
 import styles from '../styles/rocket.module.scss';
 
 const Rocket = () => {
     const scrollRef = useRef(null)
-    const { ref: aboutSectionRef, inView: aboutSectionIsVisible } = useInView();
   
   return (
-    <div ref={aboutSectionRef}>
-        <span className={aboutSectionIsVisible ? styles.animateRocket : styles.fadeOut}>
+    <div ref={useRef}>
+        <span className={scrollRef ? styles.animateRocket : styles.fadeOut}>
           🚀
         </span>
     </div>
